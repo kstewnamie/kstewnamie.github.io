ef03595680864b34c174eb456b4ee49516292ee2
gschampagne <146116523+gschampagne@users.noreply.github.com>
2026-08-11 20:57:53 -0400

    added mroe paintinf

--- art-data.js
+++ art-data.js
@@ -54,9 +54,11 @@ const ARTWORK = [
   { slug: "fresh lobster",          title: "Fresh Lobster",          size: "12 x 24", price: 200, status: "available", collections: [] },
   { slug: "mesmerized",             title: "Mesmerized",             size: "5 x 7",   price: 50,  status: "sold",      collections: ["cats"] },
   { slug: "purr purr purr",         title: "Purr Purr Purr",         size: "5 x 7",   price: 50,  status: "sold",      collections: ["cats"] },
-  { slug: "carolina wren",          title: "Carolina Wren",          size: "5 x 7",   price: 50,  status: "available",      collections: ["feathered_friends"] },
+  { slug: "carolina wren",          title: "Carolina Wren",          size: "5 x 7",   price: 50,  status: "available", collections: ["feathered_friends"] },
   { slug: "space kitty",            title: "Space Kitty",            size: "10 x 10", price: 80,  status: "sold",      collections: ["cats"] },
-  { slug: "red tufted titmouse",    title: "Red Tufted Titmouse",    size: "5 x 7",   price: 50,  status: "available",      collections: ["feathered_friends"] }
+  { slug: "red tufted titmouse",    title: "Red Tufted Titmouse",    size: "5 x 7",   price: 50,  status: "available", collections: ["feathered_friends"] },
+  { slug: "red panda",              title: "Red Panda",              size: "9 x 12",  price: 100, status: "available", collections: [] },
+  { slug: "wednesday",              title: "Wednesday",              size: "9 x 12",  price: 120, status: "available", collections: [] }
 ];
 
 
